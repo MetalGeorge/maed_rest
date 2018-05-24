c185299acfbd7c50f943661c718af3edb17da092
Jorge Escobar <jorge.escobar@truextend.com>
2018-05-23 22:34:23 -0400

    added purchase and merged verifyToken

--- serviceItems/src/app/routes/VerifyToken.js
+++ serviceItems/src/app/routes/VerifyToken.js
@@ -1,16 +1,19 @@
 var jwt = require('jsonwebtoken');
-var config = require('../../config/config');
+var config = require('../config/config');
 
 function verifyToken(req, res, next) {
-    var token = req.headers['x-access-token'];
+    var token = req.headers['authorization']
+    token = token.substring(7);
+    console.log(token);
     if (!token)
         return res.status(403).send({ auth: false, message: 'No token provided.' });
     jwt.verify(token, config.secret, function(err, decoded) {
         if (err)
             return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-        // if everything good, save to request for use in other routes
         req.userId = decoded.id;
-        req.name = decoded.name;
+        req.isSeller = decoded.isSeller;
+        req.isBuyer = decoded.isBuyer;
+        req.isAdmin = decoded.isAdmin;
         next();
     });
 }
