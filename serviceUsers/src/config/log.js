8c78da8a3c10cec1da8da629f7aec1edb40b0c30
Carlos David <carlosmontellano@gmail.com>
2018-05-25 20:33:43 -0400

    un solo log , correcciones odata

--- serviceUsers/src/config/log.js
+++ serviceUsers/src/config/log.js
@@ -4,36 +4,45 @@ const logDir = 'logs';
 const fs = require('fs');
 
 if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
- }
+    fs.mkdirSync(logDir);
+}
 
 const now = new Date();
 var logger = new(winston.Logger)({
-transports: [
+    transports: [
 
-    new winston.transports.File({
-        name: 'error-file',
-        filename: './logs/exceptions.log',
-        level: 'error',
-        json: false
-    }),
+        new(winston.transports.Console)({ level: 'warn' }),
+        new(winston.transports.File)({ filename: 'somefile.log', level: 'error' }),
 
-    new(require('winston-daily-rotate-file'))({
-        filename: './logs/users-apimodules.log',
-        timestamp: now,
-        datePattern: 'dd-MM-YYYY',
-        prepend: true,
-        json: false,
-        level: env === 'development' ? 'verbose' : 'info'
-    })
-],
-exitOnError: false
+        new winston.transports.File({
+            name: 'error-file',
+            filename: './logs/exceptions.log',
+            level: 'error',
+            json: false
+        }),
+        new winston.transports.File({
+            name: 'information-file',
+            filename: './logs/user.log',
+            level: env === 'development' ? 'verbose' : 'info',
+            json: false
+        }),
+        /*
+            new(require('winston-daily-rotate-file'))({
+                filename: './logs/users.log',
+                timestamp: now,
+                datePattern: 'dd-MM-YYYY',
+                prepend: true,
+                json: false,
+                level: env === 'development' ? 'verbose' : 'info'
+            })*/
+    ],
+    exitOnError: false
 });
 
 module.exports = logger;
 module.exports.stream = {
-  write: function(message, encoding) {
-    logger.info(message);
-    console.log('message=', message);
-  }
+    write: function(message, encoding) {
+        logger.info(message);
+        console.log('message=', message);
+    }
 };
--- serviceUsersOdata/src/index.js
+++ serviceUsersOdata/src/index.js
@@ -1,15 +1,21 @@
-var odata = require('odata-simple-server');
+var odata = require('node-odata');
 
 server = odata('mongodb://joraca:123456@ds215370.mlab.com:15370/user_75');
 
 server.resource('users', {
     name: String,
+    email: String,
     password: {
         type: String,
         select: false
-    }
+    },
+    dateCreate: String,
+    state: { type: String, enum: ['active', 'inactive'] },
+    isSeller: { type: String, enum: ['yes', 'no'] },
+    isBuyer: { type: String, enum: ['yes', 'no'] },
+    isAdmin: { type: String, enum: ['yes', 'no'] }
 });
 
-server.listen(3000, function() {
-    console.log('OData services has started, you can visit by http://localhost:3000/users');
+server.listen(3001, function() {
+    console.log('OData services has started, you can visit by http://localhost:3001/users');
 });
