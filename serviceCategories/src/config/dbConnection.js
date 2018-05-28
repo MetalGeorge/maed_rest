0dcb07460c230d5b22138c8a312f9c4e0befad18
znarf1587 <fferrufino.rq@gmail.com>
2018-05-27 20:12:58 -0400

    modify data base connection

--- serviceCategories/src/config/dbConnection.js
+++ serviceCategories/src/config/dbConnection.js
@@ -1,12 +1,9 @@
 const mysql = require('mysql');
 
 let pool = mysql.createPool({
-    // host: "34.207.70.205",
-    // user: "devroot",
-    // password: "devroot.123",
-    host: "localhost",
-    user: "root",
-    password: "",
+    host: "34.207.70.205",
+    user: "devroot",
+    password: "devroot.123",
     database: 'dbtantakatu'
 });
 
