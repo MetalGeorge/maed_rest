7c5a5ed0e01b1d3337016f8e3dff4b0a46ab5fa0
Carlos David <carlosmontellano@gmail.com>
2018-05-28 02:23:52 -0400

    autenticacion en odata

--- serviceUsers/src/routes/UserController.js
+++ serviceUsers/src/routes/UserController.js
@@ -86,6 +86,7 @@ router.delete('/:id', VerifyToken, function(req, res, next) {
 // UPDATES A SINGLE USER IN THE DATABASE
 router.put('/:id', VerifyToken, function(req, res) {
     logger.info("Begin Update User");
+    console.log(req);
     User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(err, user) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
--- serviceUsersOdata/src/index.js
+++ serviceUsersOdata/src/index.js
@@ -1,4 +1,7 @@
 var odata = require('node-odata');
+var jwt = require('jsonwebtoken');
+var config = require('./config/config');
+
 
 server = odata('mongodb://joraca:123456@ds215370.mlab.com:15370/user_75');
 
@@ -14,7 +17,31 @@ server.resource('users', {
     isSeller: { type: String, enum: ['yes', 'no'] },
     isBuyer: { type: String, enum: ['yes', 'no'] },
     isAdmin: { type: String, enum: ['yes', 'no'] }
+}).list().auth(function(req) {
+    var token = req.headers['authorization']
+    if (!token) {
+        console.log('No hay token');
+        return false;
+    } else {
+        token = token.substring(7);
+        jwt.verify(token, config.secret, function(err, decoded) {
+            if (err) valor = false
+            else {
+                req.userId = decoded.id;
+                req.isSeller = decoded.isSeller;
+                req.isBuyer = decoded.isBuyer;
+                req.isAdmin = decoded.isAdmin;
+                console.log('logro autenticar');
+                if (decoded.isAdmin == 'yes')
+                    valor = true;
+                else
+                    valor = false;
+            }
+        });
+        return valor
+    }
 });
+
 
 server.listen(3001, function() {
     console.log('OData services has started, you can visit by http://localhost:3001/users');
